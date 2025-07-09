9c3bcc47707408bda8b29135a17556dad13b16fb
Alfredo Romero Gonzalez <fredyglz.fr@gmail.com>
2025-07-08 21:38:39 -0600

    Sección 13 - Creando el admin

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import AuthLayout from "./layouts/AuthLayout";
+import AppLayout from "./layouts/AppLayout";
 import LoginView from "./views/LoginView";
 import RegisterView from "./views/RegisterView";
-import AuthLayout from "./layouts/AuthLayout";
+import LinkTreeView from "./views/LinkTreeView";
+import ProfileView from "./views/ProfileView";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Auth */}
         <Route element={<AuthLayout />}>
           <Route path="/auth/login" element={<LoginView />} />
           <Route path="/auth/register" element={<RegisterView />} />
         </Route>
+
+        {/* Dashboard */}
+        <Route path="/admin" element={<AppLayout />}>
+          <Route index element={<LinkTreeView />} />
+          <Route path="profile" element={<ProfileView />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/auth/login" />} />
       </Routes>
     </BrowserRouter>
   );
